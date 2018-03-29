6d5d6b2353ba6c5f91b8a88caebcf1a2c61c9308
hotamisli <salihahotamisli@gmail.com>
2018-03-29 16:44:03 +0000

    initial

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
-//app.set('port', 3000);
+
+var port = process.env.PORT || 3000;
+app.set('port', 3000);
 app.get('/', function(req, res) {
     res.send('welcome to my homepage');
 });
-app.listen(3000, function() {
-    console.log("my webpage is served on port ", app.get('port'));
+app.listen(port, function() {
+    console.log("my webpage is served on port ", port);
 });
